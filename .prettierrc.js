2a57155987624205d425a923446f73a50d66d84b
Diego Fernando Carrión <diego@crthaze.com>
2022-06-27 14:04:05 +0200

    Setup basic repository structure and build system

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+	...require('@spotify/prettier-config'),
+	useTabs: true, // https://web.archive.org/web/20220616035943/https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/
+
+};
---
+++ index.ts
@@ -0,0 +1,6 @@
+module.exports = {
+	openSLO: require('./openslo'),
+	openSLOBackend: require('./openslo-backend'),
+	openSLOCommon: require('./openslo-common'),
+	openSLODataSourcePrometheus: require('./openslo-datasource-prometheus'),
+};
