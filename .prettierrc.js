e65441e8e569266482b37f3a3a558a1918d46e8b
Jakub Warczarek <jakub.warczarek@gmail.com>
2022-11-08 16:07:08 +0100

    Bump dependencies (#12)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,4 @@
 module.exports = {
 	...require('@spotify/prettier-config'),
 	useTabs: true, // https://web.archive.org/web/20220616035943/https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/
-
 };
